907dae4cd31e88b368a8dda13e6b748a22743df8
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-10-14 11:36:53 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SFExtractor
+Simple tool to extract SF Symbols on mac
+## Usage
+Building:
+```bash
+swiftc SFExtractor.swift -o sfe
+```
+Extracting specific symbol:
+```bash
+./sfe -s <name>
+```
+Extracting all:
+```bash
+./sfe
+```
+Usage:
+```bash
+./sfe -h
+```
--- SFExtractor.swift
+++ SFExtractor.swift
@@ -1,22 +1,55 @@
-/*
-Usage: swift SFExtractor <symbol_name>
-*/
-
 import Foundation
 import AppKit
 
+let redColor = "\u{001B}[31m"
+let orangeColor = "\u{001B}[33m"
+let greenColor = "\u{001B}[32m"
+let resetColor = "\u{001B}[0m"
+
 let arguments = CommandLine.arguments
-if arguments.count < 2 {
-    print("Usage: SFExtractor.swift <symbol_name>")
+
+if arguments.contains("-h") {
+    print("Usage:")
+    print("  -s <symbol_name> : Extract a specific symbol")
+    print("  -h : Show help")
+    print("  (No options) : Extract all symbols until the program is shut down")
+    exit(0)
+}
+
+let fileManager = FileManager.default
+let binDirectoryURL = URL(fileURLWithPath: "bin")
+do {
+    try fileManager.createDirectory(at: binDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+} catch {
+    print("\(redColor)Failed to create 'bin/' directory: \(error)\(resetColor)")
     exit(1)
 }
 
-let symbolName = arguments[1]
+if let symbolIndex = arguments.firstIndex(of: "-s"), symbolIndex + 1 < arguments.count {
+    let symbolName = arguments[symbolIndex + 1]
+    extractSymbol(symbolName)
+} else {
+    var symbolNames: [String] = []
 
-if let image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "") {
-    if let tiffData = image.tiffRepresentation, let cgImage = NSBitmapImageRep(data: tiffData)?.cgImage {
-        _ = cgImage.width
-        _ = cgImage.height
+    if let symbolNamesURL = Bundle.main.url(forResource: "symbol_names", withExtension: "txt"),
+        let symbolNamesText = try? String(contentsOf: symbolNamesURL, encoding: .utf8) {
+        symbolNames = symbolNamesText.components(separatedBy: .newlines)
+    } else {
+        print("\(orangeColor)Warning: Failed to load symbol names from the text file. Skipping to the next symbol.\(resetColor)")
+    }
+    
+    for symbolName in symbolNames {
+        extractSymbol(symbolName)
+    }
+}
+
+func extractSymbol(_ symbolName: String) {
+    if let image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "") {
+        guard let tiffData = image.tiffRepresentation, !tiffData.isEmpty else {
+            print("\(redColor)Error: Failed to extract the SF Symbol tiff representation for symbol: \(symbolName)\(resetColor)")
+            return
+        }
+
         let targetSize = NSSize(width: 200, height: 200)
 
         let resizedImage = NSImage(size: targetSize)
@@ -25,23 +58,20 @@ if let image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "
         image.draw(in: NSRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height))
         resizedImage.unlockFocus()
 
+        let outputDirectoryURL = URL(fileURLWithPath: "bin")
+        let outputFileURL = outputDirectoryURL.appendingPathComponent("\(symbolName).png")
+
         if let pngData = resizedImage.tiffRepresentation {
             do {
-                try pngData.write(to: URL(fileURLWithPath: "\(symbolName).png"))
-                print("SF Symbol extracted and saved as \(symbolName).png (200x200)")
+                try pngData.write(to: outputFileURL)
+                print("\(greenColor)Success: SF Symbol extracted and saved as bin/\(symbolName).png (200x200)\(resetColor)")
             } catch {
-                print("Failed to write PNG data to file: \(error)")
-                exit(1)
+                print("\(redColor)Error: Failed to write PNG data to file for symbol: \(symbolName) - \(error)\(resetColor)")
             }
         } else {
-            print("Failed to extract the SF Symbol tiff representation.")
-            exit(1)
+            print("\(redColor)Error: Failed to extract the SF Symbol tiff representation for symbol: \(symbolName)\(resetColor)")
         }
     } else {
-        print("Failed to extract the SF Symbol CGImage.")
-        exit(1)
+        print("\(orangeColor)Warning: SF Symbol not found: \(symbolName). Skipping to the next symbol.\(resetColor)")
     }
-} else {
-    print("SF Symbol not found.")
-    exit(1)
 }
